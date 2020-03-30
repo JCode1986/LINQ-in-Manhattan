add7da84d1f9d00487e37b3ca83285930877523e
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-30 00:24:48 -0700

    created classes for JSON object, and able read object with newtonsoft.

--- LinqInManhattan/Program.cs
+++ LinqInManhattan/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LinqInManhattan
 {
     class Program
     {
+        public static string path = "../../../../../data.json";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            JObject CSharpObj = JObject.Parse(File.ReadAllText(@"../../../../data.json"));
+            Console.WriteLine(CSharpObj);
         }
+        
     }
 }
+
