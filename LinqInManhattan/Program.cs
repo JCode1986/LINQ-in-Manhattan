da266b4b212aa4aa362a9a766f8010ec4acab895
Joseph Hangarter <joseph.hangarter@yahoo.com>
2020-03-30 23:12:43 -0700

    all methods completed and updated readme

--- LinqInManhattan/Program.cs
+++ LinqInManhattan/Program.cs
@@ -9,19 +9,81 @@ namespace LinqInManhattan
 {
     class Program
     {
-        public static string path = "../../../../data.json";
-        public static JObject CSharpObj = JObject.Parse(File.ReadAllText(@path));
         static void Main(string[] args)
         {
-            OutputAllNeighborhoods(NeighborhoodList());
-            OutputAllNeighborhoodsThatHasNames(NeighborhoodList());
-            RemoveDuplicateNeighborhoods(NeighborhoodList());
-            //SingleQueryFromPreviousMethods(NeighborhoodList());
+            Start();
         }
 
+        /// <summary>
+        /// Starts the app, and gives options to user
+        /// </summary>
+        public static void Start()
+        {
+            Console.WriteLine("\nManhattan Neighborhood List:\n");
+            Console.WriteLine("'1' - Output all neighborhoods");
+            Console.WriteLine("'2' - Output all neighborhoods with names");
+            Console.WriteLine("'3' - Output all unique neighborhoods");
+            Console.WriteLine("'4' - Output previous methods in single query");
+            Console.WriteLine("'5' - Rewrote option '2' using the opposing method");
+            Console.WriteLine("'x' - Exit");
+            Console.Write("\nWhat would you like to do?: ");
+            string input = Console.ReadLine();
+            Console.Clear();
+
+            switch (input)
+            {
+                case "1":
+                    OutputAllNeighborhoods(NeighborhoodList());
+                    break;
+                case "2":
+                    OutputAllNeighborhoodsThatHasNames(NeighborhoodList());
+                    break;
+                case "3":
+                    RemoveDuplicateNeighborhoods(NeighborhoodList());
+                    break;
+                case "4":
+                    SingleQueryFromPreviousMethods(NeighborhoodList());
+                    break;
+                case "5":
+                    OutPutAllNeighborhoodWithNamesOpposingMethod(NeighborhoodList());
+                    break;
+                case "x":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.Clear();
+                    Start();
+                    break;
+            }
+            Start();
+        }
+
+        /// <summary>
+        /// Returns string of contents in list
+        /// </summary>
+        /// <param name="array"></param>
+        public static void PrintList(IEnumerable<string> array)
+        {
+            string result = "";
+            foreach (var property in array)
+            {
+                result += $"{property}, ";
+            }
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// stores all the neighborhoods from json into a list
+        /// </summary>
+        /// <returns></returns>
         public static List<Properties> NeighborhoodList()
         {
+            string path = "../../../../data.json";
+
+            JObject CSharpObj = JObject.Parse(File.ReadAllText(@path));
+
             List<Properties> neighborhoodList = new List<Properties>();
+
             foreach (var props in CSharpObj["features"])
             {
                 Properties neighborhood = new Properties()
@@ -33,22 +95,31 @@ namespace LinqInManhattan
             return neighborhoodList;
         }
 
-    /// <summary>
-    /// Output all of the neighborhoods in data list (Final Total: 147 neighborhoods)
-    /// </summary>
-    /// <returns></returns>
-    public static void OutputAllNeighborhoods(List<Properties> neighborhoodList)
+        //Question 1: Output all of the neighborhoods in this data list (Final Total: 147 neighborhoods)
+
+        /// <summary>
+        /// Returns all the neighborhoods
+        /// </summary>
+        /// <returns></returns>
+        public static void OutputAllNeighborhoods(List<Properties> neighborhoodList)
         {
-            Console.WriteLine($"There are {neighborhoodList.Count} total neighborhoods which are:\n");
+            Console.WriteLine($"\nThere are {neighborhoodList.Count} total neighborhoods which are:\n");
+
+            string result = "";
 
             foreach (var property in neighborhoodList)
             {
-                Console.WriteLine(property.Neighborhood);
+                result += $"{property.Neighborhood}, ";
             }
+
+            Console.WriteLine(result);
+
         }
 
+        //Filter out all the neighborhoods that do not have any names (Final Total: 143)
+
         /// <summary>
-        /// Filter out all the neighborhoods that do not have any names (Final Total: 143)
+        /// Returns neighborhoods with names
         /// </summary>
         public static void OutputAllNeighborhoodsThatHasNames(List<Properties> neighborhoodList)
         {
@@ -57,16 +128,15 @@ namespace LinqInManhattan
                         where p.Neighborhood != ""
                         select p.Neighborhood;
 
-            Console.WriteLine($"There are {queryWithNames.Count()} total neighborhoods with names which are:\n");
+            Console.WriteLine($"\nThere are {queryWithNames.Count()} total neighborhoods with names which are:\n");
 
-            foreach (var neighborhood in queryWithNames)
-            {
-                 Console.WriteLine(neighborhood);
-            }
+            PrintList(queryWithNames);
         }
 
+        //Question 3: Remove the duplicates (Final Total: 39 neighborhoods)
+
         /// <summary>
-        /// Remove the duplicates (Final Total: 39 neighborhoods)
+        /// Removes all duplicate neighborhoods
         /// </summary>
         public static void RemoveDuplicateNeighborhoods(List<Properties> neighborhoodList)
         {
@@ -77,13 +147,12 @@ namespace LinqInManhattan
 
             var NoDupes = queryWithNames.GroupBy(n => n).Select(p => p.First());
 
-            Console.WriteLine($"There are {NoDupes.Count()} total neighborhoods that are unique which are:\n");
+            Console.WriteLine($"\nThere are {NoDupes.Count()} total neighborhoods that are unique which are:\n");
 
-            foreach (var neighborhood in NoDupes)
-            {
-                Console.WriteLine(neighborhood);
-            }
+            PrintList(NoDupes);
         }
+
+        //Question 4: Rewrite the queries from above and consolidate all into one single query
 
         /// <summary>
         /// Filter out neighborhoods with no names and remove duplicates
@@ -91,7 +160,29 @@ namespace LinqInManhattan
         /// <param name="neighborhoodList"></param>
         public static void SingleQueryFromPreviousMethods(List<Properties> neighborhoodList)
         {
-            
+            var chainingQuery = neighborhoodList
+                    .Where(neighbor => neighbor.Neighborhood != "")
+                    .Select(neigbor => neigbor.Neighborhood)
+                    .Distinct();
+
+            Console.WriteLine($"\nThere are {chainingQuery.Count()} total neighborhoods that are unique which are:\n");
+
+            PrintList(chainingQuery);
+        }
+
+        //Question 5: Rewrite at least one of these questions only using the opposing method (example: Use LINQ Query statements instead of LINQ method calls and vice versa.)
+       
+        /// <summary>
+        /// Rewrite at least one of these questions only using the opposing method
+        /// </summary>
+        /// <param name="neighborhoodList"></param>
+        public static void OutPutAllNeighborhoodWithNamesOpposingMethod(List<Properties> neighborhoodList)
+        {
+            var queryWithNames = neighborhoodList.Where(p => p.Neighborhood != "").Select(p => p.Neighborhood);
+
+            Console.WriteLine($"\nThere are {queryWithNames.Count()} total neighborhoods that are unique which are:\n");
+
+            PrintList(queryWithNames);
         }
     }
 }
